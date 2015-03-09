b36802e168ad55ee5cde08f75ae6351b5842937b
unknown <logan@DRAU-THINK.cleverex.com>
2015-03-09 16:00:53 -0400

    handled post and get requests, added to the request object, handled user creationg

--- app/app.js
+++ app/app.js
@@ -7,13 +7,11 @@ var User = Backbone.Model.extend({
 	}
 });
 
-var logan = new User({ firstName : "Logan", lastName : "Sobczak" });
-var lauren = new User({ firstName : "Lauren", lastName : "McLeod" });
-
+/*
 var Users = Backbone.Collection.extend({
 	url : "/",
 	model : User
 });
 
 var users = new Users([logan, lauren]);
-users.sync("create", users);
+users.sync("create", users);*/
--- server.js
+++ server.js
@@ -9,53 +9,14 @@ var http = require('http'),
 var server = http.createServer( function (request, response) {
 
 	// Gets the parsed url, path, and file extension
-	var parsedURL = url.parse(request.url, true),
-		pathname = parsedURL.pathname,
-		ext = path.extname(pathname).replace(".","");
+	request.route = (url.parse(request.url, true)).pathname,
+	request.ext = path.extname(request.route);
 
-	if (request.method === "POST") {
-		var fullBody = "";
+	//console.dir(request.route + " : " + request.ext);
 
-		// Parse the request
-		request.on('data', function (chunk) {
-			fullBody += chunk;
-			// Flood detection
-			if (fullBody.length > 1e6) request.connection.destroy();
-		});
+	// EXT MAP
 
-		// Save the request
-		request.on('end', function () {
-			var post = fullBody,
-				options = {};
-			fs.stat(__dirname + '/data/data.json', function (err, stats) {
-				if (err) console.error(err.message);
-				if (stats) { // The file already exists
-					options = { flags : 'r', encoding : 'utf-8'};
-				} else { // The file does not exist
-					options = { flags : 'w', encoding : 'utf-8'};
-				}
-			});
-			// Add to existing data
-			var store = fs.readFile('data/data.json', function (err, data) {
-				if (err) console.error(err.message); // Error reading the store !!
-				// Handle error
-				var store = JSON.parse(data);
-				console.log(JSON.parse(post));
-				store.users = [];
-				store.users.push(post);
-				fs.writeFile('data/data.json', JSON.stringify(store), options, function (err) {
-					if (err) console.error(err.message);
-				});
-			});
-		});
-	}
-	// The default route
-	if (pathname === "\/") {
-		pathname = "/index.html",
-		ext = "html";
-	}
-
-	var map = {
+	request.extMap = {
 	  '.ico': 'image/x-icon',
 	  '.html': 'text/html',
 	  '.js': 'text/javascript',
@@ -64,24 +25,100 @@ var server = http.createServer( function (request, response) {
 	  '.png': 'image/png'
 	};
 
-	// Create a read stream
-	var rs = fs.createReadStream(__dirname + pathname);
+	// ROUTES
 
-	 // Read stream failed to read from path / 404 for html pages
+	// Default
+	if (request.route === "\/") {
+		request.route = "/index.html",
+		request.ext = "html";
+	}
+
+	// REQUESTS
+
+	if (request.method === "POST") _POST(request, response);
+	if (request.method === "GET") _GET(request, response);
+
+});
+
+// START THE SERVER
+
+server.listen(port);
+console.log("Listening on port " + port + " . . .");
+
+
+function _POST (request, response) {
+
+	var fullBody = "";
+
+	// Parse the request
+	request.on('data', function (chunk) {
+		fullBody += chunk;
+		// Flood detection
+		if (fullBody.length > 1e6) request.connection.destroy();
+	});
+
+
+	// Create or update the request
+	request.on('end', function () {
+
+		// POST ROUTES
+
+		if (request.route = "/createUser") {
+
+			var options = {};
+			fs.stat(__dirname + '/data/data.json', function (err, stats) {
+				if (err) console.error(err.message);
+				if (stats) { // The file already exists
+					options = { flags : 'r', encoding : 'utf-8'};
+				} else { // The file does not exist
+					options = { flags : 'w', encoding : 'utf-8'};
+				}
+			});
+
+			var store = fs.readFile('data/data.json', function (err, data) {
+				if (err) console.error(err.message); // Error reading the store !!
+
+				// Handle error
+
+				var store = (data.length) ? JSON.parse(data) : {};
+				store.users = store.users || [];
+				store.users.push(qs.parse(fullBody));
+				fs.writeFile('data/data.json', JSON.stringify(store), options, function (err) {
+					if (err) console.error(err.message);
+				});
+			});
+		}
+	});
+}
+
+function _GET(request, response) {
+
+	var fullBody = "";
+
+	// Create a read stream
+	var rs = fs.createReadStream(__dirname + request.route);
+
+	// Read stream failed to read from path / 404 for html pages
 	rs.on('error', function (err) {
 		console.error(err.message);
-		if (ext === "html") {
-			response.writeHead(404, {'Content-type': map[ext]});
-			response.end("<p>" + err.message + "</p><p>404::< " + pathname + " >::not found.</p>");
+		response.writeHead(404, {'Content-type' : request.extMap[request.ext]});
+		if (request.ext === ".html") {
+			response.end("<p>" + err.message + "</p><p>404::< " + request.route + " >::not found.</p>");
 		} else {
-			response.writeHead(404);
 			response.end();
 		}
 	});
 
-	// Pipe the response to the client
-	rs.pipe(response);
-});
+	// Read the stream
+	rs.on('data', function (chunk) {
+		fullBody += chunk;
+		// Flood detection
+		if (fullBody.length > 1e6) request.connection.destroy();
+	});
 
-server.listen(port);
-console.log("Listening on port " + port + " . . .");
+	// Pipe the response to the client
+	rs.on('end', function () {
+		response.writeHead(200, {'Content-type' : request.extMap[request.ext]});
+		response.end(fullBody);
+	});
+}
