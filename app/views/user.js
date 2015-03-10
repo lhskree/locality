8c5d8050457c60b60d03fd7ededf09cab7826d45
unknown <logan@DRAU-THINK.cleverex.com>
2015-03-10 13:14:00 -0400

    getting down to templates

--- app/views/user.js
+++ app/views/user.js
@@ -1,5 +1,9 @@
 Locality = Locality || {};
 
 Locality.Views.User = Backbone.View.extend({
-	el : ".user"
+	el : ".user",
+	template : "<p>Ok</p>",
+	render : function (model) {
+		return this.template;
+	}
 });
--- app/views/users.js
+++ app/views/users.js
@@ -2,7 +2,14 @@ Locality = Locality || {};
 
 Locality.Views.Users = Backbone.View.extend({
 	el : '.users',
-	render : function () {
-		this.$el.html("Hello!");
+	render : function (collection) {
+		this.$el.html(this.renderChildren(collection));
+	},
+	renderChildren : function (collection) {
+		var which = this;
+		collection.models.forEach(function (model) {
+			var userView = new Locality.Views.User();
+			which.$el.append(userView.render(model));
+		});
 	}
 });
