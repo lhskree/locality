3c64332c6f6a283d31e9dbf38184b3aa9027876a
unknown <logan@DRAU-THINK.cleverex.com>
2015-03-13 09:31:01 -0400

    now the views render in the outlet

--- app/views/users.js
+++ app/views/users.js
@@ -4,7 +4,7 @@ Locality.Views.Users = Backbone.View.extend({
 	className : 'users',
 	render : function () {
 		var compiled = _.template($("#users-template").html());
-		this.$el.html(compiled(this.collection));
+		this.$el.html(compiled({models:this.collection.toJSON()}));
 		return this.el;
 	},
 });
